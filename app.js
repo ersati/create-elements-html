db222194866efc4675de2174ed76363a228fa384
ersati <piotrp92@gmail.com>
2021-03-31 18:04:43 +0100

    add functinality do add Form

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 const addButton = document.querySelector('.form--add .form__submit');
-console.log(addButton)
-const createElements = (e, node, txt, attr, value) => {
+const addForm = document.querySelector('.form--add');
+
+function createElements(e, node = "div", text = "tekst", attr = "class", value = "big") {
     e.preventDefault();
-    const element = createElements(node);
-    const text = element.createTextNode(txt);
+    const element = document.createElement(node);
+    element.textContent = text;
+    console.log(element)
     element.setAttribute(attr, value);
-    // console.log(element)
+
+    
     console.log(e)
     console.log('ok')
 }
 
 
 
-addButton.addEventListener('submit', (e)=> createElements(e, 
+addForm.addEventListener('submit', (e) => createElements(e,
     document.querySelector('input[name=node]').value,
-document.querySelector('input[name=txt]').value,
-document.querySelector('input[name=attr]').value,
-document.querySelector('input[name=value]').value))
+    document.querySelector('input[name=txt]').value,
+    document.querySelector('input[name=attr]').value,
+    document.querySelector('input[name=value]').value))
+
+const searchForm = document.querySelector(".form--search");
+
+function searchElements(e) {
+    e.preventDefault()
+    console.log(e)
+}
+
+searchForm.addEventListener('submit', (e) => {
+    searchElements(e)
+})
