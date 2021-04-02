8b373ca4c76745050032741cbc79b5b3fb3f4d0a
ersati <piotrp92@gmail.com>
2021-04-02 01:53:24 +0100

    add validation

--- app.js
+++ app.js
@@ -5,15 +5,25 @@ const createResult = document.querySelector('.container--info')
 
 function createElements(e, node, text, attr, value) {
     e.preventDefault();
+    console.log(node,text,attr,value)
     const element = document.createElement(node);
-    const textNode = document.createTextNode(text);
-    element.appendChild(textNode)
-    console.log(element)
-    element.setAttribute(attr, value);
+  
+    if(text !== ''){
+        const textNode = document.createTextNode(text);
+        element.appendChild(textNode)
+
+    }
+    if(attr !== '' || value !== ""){
+        element.setAttribute(attr, value);
+
+    }
 
     createResult.appendChild(element)
-    console.log(e)
-    console.log('ok')
+   
+    document.querySelector('input[name=node]').value = ''
+    document.querySelector('input[name=txt]').value = ''
+    document.querySelector('input[name=attr]').value = ''
+    document.querySelector('input[name=value]').value = ''
 }
 
 
@@ -29,21 +39,26 @@ const searchForm = document.querySelector(".form--search");
 function searchElements(e, element) {
     e.preventDefault()
     const allElements = createResult.querySelectorAll(element)
-    console.log(allElements)
     console.log(e)
-
+if(allElements.length === 0 ){
+    searchResult.innerHTML = `<p>No data, please check diffrent element</p>`
+} else {
     allElements.forEach(element => {
         console.log(element.className)
         console.log(element.offsetHeight)
-        showElements(element.className, element.offsetHeight)
+        showElements(element.className, element.offsetHeight, element.id)
     })
 }
-
-function showElements(className, offset){
+document.querySelector('input[name=search]').value = ''
+}
+function showElements(className, offset, id){
     const classNameParagraph = `<p>Name of Class: ${className}</p>`
     const offsetParagraph = `<p>Height of element: ${offset}</p>`
+    const idParagraph = `<p>Name of id: ${id}</p>`;
     searchResult.innerHTML += classNameParagraph;
-    searchResult.innerHTML += offsetParagraph;
+    searchResult.innerHTML += offsetParagraph,
+    searchResult.innerHTML += idParagraph;
+    
 
 }
 searchForm.addEventListener('submit', (e) => {
